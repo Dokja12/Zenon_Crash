08d7bc5890dd29b6c0b4253b58326afd233ce37f
Dokja12 <letueursidi166@gmail.com>
2024-09-18 15:30:24 +0000

    Update settings.js

--- settings.js
+++ settings.js
@@ -4,14 +4,14 @@ const fs = require('fs')
 const chalk = require('chalk')
 
 //contact details
-global.ownernomer = "48459439078"
-global.ownername = "Toxxic Md"
+global.ownernomer = "2250100113543"
+global.ownername = "Dokja.L"
 global.ytname = "Toxxic Md"
 global.socialm = "GitHub: Toxic1239"
 global.location = "Africa, Nigeria, Lagos"
 
-global.ownernumber = '48459439078'  //creator number
-global.ownername = 'Toxxic Md' //owner name
+global.ownernumber = '2250100113543'  //creator number
+global.ownername = 'Dokja.L' //owner name
 global.botname = 'ᴢᴇɴᴏɴ ᴍᴅ' //name of the bot
 
 //sticker details
